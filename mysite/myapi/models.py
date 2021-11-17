a0a3ffdad21a058e4c2db91e17f435fc60baef40
Emily <emilypbi@gmail.com>
2021-11-17 21:14:11 +0000

    Change image field type.

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ImagePost(models.Model):
-    image = models.CharField(max_length=60)
+    image = models.ImageField(upload_to='images')
     caption = models.TextField(max_length=100)
     author = models.CharField(max_length=30)
     likes = models.IntegerField(default=0)
