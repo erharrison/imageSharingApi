e0587f367324563d84656adf42b331a42bf26d8b
Emily <emilypbi@gmail.com>
2021-11-17 20:47:19 +0000

    Add view for rendering post as JSON.

--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ImagePostSerializer
+from .models import ImagePost
 
-# Create your views here.
+
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = ImagePost.objects.all().order_by('image')
+    serializer_class = ImagePostSerializer
