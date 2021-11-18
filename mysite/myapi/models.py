9febe6875cc07d4cac677a4f8752746d96777154
Emily <emilypbi@gmail.com>
2021-11-18 18:48:54 +0000

    Add datetime now to posts, so they can be filtered by upload date.

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
 
 
 class ImagePost(models.Model):
@@ -7,6 +9,18 @@ class ImagePost(models.Model):
     author = models.CharField(max_length=30)
     likes = models.IntegerField(default=0)
     dislikes = models.IntegerField(default=0)
+    upload_date = models.DateTimeField(default=datetime.now, blank=True)
 
     def __str__(self):
         return self.caption
+
+
+class User(models.Model):
+    following = models.ManyToManyField(
+        "self", blank=True, related_name="followers", symmetrical=False
+    )
+    username = models.TextField(max_length=30, blank=True, default="username")
+    website = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return f"{self.username} Profile"
--- mysite/myapi/views.py
+++ mysite/myapi/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ImagePostSerializer
-from .models import ImagePost
+from .models import ImagePost, User
 from .forms import ImageForm
+
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 class ImagePostViewSet(viewsets.ModelViewSet):
@@ -21,3 +25,26 @@ def image_upload_view(request):
     else:
         form = ImageForm()
     return render(request, 'index.html', {'form': form})
+
+
+def profile(request, username):
+    user_profile = User.objects.get(username=username)
+
+    data = {
+        "author": user_profile,
+    }
+    return render(request, "author/profile.html", data)
+
+
+def follow_toggle(request, author):
+    author_obj = User.objects.get(username=author)
+    current_user_obj = User.objects.get(username=request.user.username)
+    following = author_obj.following.all()
+
+    if author != current_user_obj.username:
+        if current_user_obj in following:
+            author_obj.following.remove(current_user_obj.id)
+        else:
+            author_obj.following.add(current_user_obj.id)
+
+    return HttpResponseRedirect(reverse(profile, args=[author_obj.username]))
